fbb08b1af1b05a51e0343267fdebe706e6538f29
Rebekah-Garrett <rebekahlgarrett@gmail.com>
2025-01-25 13:21:12 -0500

    feat: wrote scoring command

--- src/main/java/frc/robot/commands/ScoringCommand.java
+++ src/main/java/frc/robot/commands/ScoringCommand.java
@@ -1,5 +1,49 @@
 package frc.robot.commands;
 
-public class ScoringCommand {
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ScoringSubsystem;
 
+public class ScoringCommand extends Command {
+
+    private ScoringSubsystem m_ScoringSubsystem;
+    private boolean intake;
+    private Timer outtakeTimer;
+
+    public ScoringCommand(ScoringSubsystem scoringSub, boolean intake) {
+        m_ScoringSubsystem = scoringSub;
+        addRequirements(scoringSub);
+        this.intake = intake;
+        outtakeTimer = new Timer();
+    }
+
+    @Override
+    public void initialize() {
+        if (intake) {
+            m_ScoringSubsystem.runIntake();
+        } else {
+            m_ScoringSubsystem.runOuttake();
+            outtakeTimer.start();
+        }
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (intake) {
+            return m_ScoringSubsystem.coralSensor.get();
+        } else {
+            return outtakeTimer.hasElapsed(6);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_ScoringSubsystem.stopMotors();
+        outtakeTimer.reset();
+    }
 }
