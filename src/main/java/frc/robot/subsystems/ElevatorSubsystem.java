3e60a7cefb122bc7c4b25a5b80873c809fc2a94d
Rebekah-Garrett <rebekahlgarrett@gmail.com>
2025-02-01 11:29:30 -0500

    feat: Added setpoint command to elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,27 +3,60 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
-    private SparkMax motor1;
-    private SparkMax motor2;
+    private SparkMax motorL;
+    private SparkMax motorR;
+    private DutyCycleEncoder encoderL = new DutyCycleEncoder(2);
+    private DutyCycleEncoder encoderR = new DutyCycleEncoder(3);
+    private double kp = 0.1, ki = 0, kd = 0;
+    private PIDController elevatorPID = new PIDController(kp, ki, kd);
 
     public ElevatorSubsystem() {
-        motor1 = new SparkMax(26, MotorType.kBrushless);
-        motor2 = new SparkMax(27, MotorType.kBrushless);
+        motorL = new SparkMax(26, MotorType.kBrushless);
+        motorR = new SparkMax(27, MotorType.kBrushless);
+    }
+
+    private double getDesiredPosistion(int height) {
+        switch (height) {
+            case 0:
+                return 0;
+            case 1:
+                return 0;
+            case 2:
+                return 0;
+            case 3:
+                return 0;
+            default:
+                return 0;
+        }
+
     }
 
     public Command manualElevatorCommand(double input) {
         return this.startEnd(() -> {
-            motor1.set(input);
-            motor2.set(input);
+            motorL.set(input);
+            motorR.set(input);
         }, () -> {
-            motor1.set(0);
-            motor2.set(0);
+            motorL.set(0);
+            motorR.set(0);
         });
 
+    }
+
+    public Command setPointElevatorCommand(int height) {
+        return this.runEnd(() -> {
+            elevatorPID.setSetpoint(getDesiredPosistion(height));
+            motorL.set(elevatorPID.calculate(encoderL.get()));
+            motorR.set(elevatorPID.calculate(encoderR.get()));
+        }, () -> {
+            motorL.set(0);
+            motorR.set(0);
+        });
     }
 }
