d2269700b4f4e46488fc8ea2bd2e7a4207af0189
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2025-03-07 00:05:33 -0500

    chore: make the climber use velocity

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -10,10 +10,12 @@ import static edu.wpi.first.units.Units.Degrees;
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.configs.AudioConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CanConstants;
@@ -23,18 +25,18 @@ public class ClimberSubsystem extends SubsystemBase {
 	private final TalonFX beefyMotor;
 	private final Orchestra marioUnderwater;
 	private final Orchestra duckOrchestra;
-	private static final Angle ROTATIONS_DOWN = Angle.ofBaseUnits(60, Degrees);
+	private static final Angle ROTATIONS_DOWN = Angle.ofBaseUnits(60 * 450, Degrees);
 	private static final Current CURRENT_THRESHOLD = Current.ofBaseUnits(40, Amps);
-	private final PositionVoltage positionVoltage = new PositionVoltage(0);
 
 	public ClimberSubsystem() {
 		beefyMotor = new TalonFX(CanConstants.beefyMotor);
 
 		AudioConfigs audioConfigs = new AudioConfigs().withAllowMusicDurDisable(true);
 		TalonFXConfiguration config = new TalonFXConfiguration().withAudio(audioConfigs);
+
 		config.Slot0.kP = 0.11;
-		config.Slot0.kI = 0.5;
-		config.Slot0.kD = 0.0001;
+		config.Slot0.kI = 0;
+		config.Slot0.kD = 0;
 		beefyMotor.getConfigurator().apply(config);
 
 		beefyMotor.setPosition(0);
@@ -49,32 +51,34 @@ public class ClimberSubsystem extends SubsystemBase {
 		duckOrchestra.loadMusic("music/duck.chrp");
 	}
 
-	public Command climbCommand() {
-		return run(() -> {
-			Angle currentPosition = beefyMotor.getPosition().getValue();
-			Current current = beefyMotor.getSupplyCurrent().getValue();
+	@Override
+	public void periodic() {
+		SmartDashboard.putNumber("climber/climber speed", beefyMotor.get());
+		SmartDashboard.putNumber("climber/climber current", beefyMotor.getSupplyCurrent().getValueAsDouble());
+		SmartDashboard.putNumber("climber/climber temp", beefyMotor.getDeviceTemp().getValueAsDouble());
+	}
 
-			if (currentPosition.compareTo(ROTATIONS_DOWN) > 0) {
-				beefyMotor.setControl(positionVoltage.withPosition(-ROTATIONS_DOWN.baseUnitMagnitude())); // Go down
-			} else if (current.compareTo(CURRENT_THRESHOLD) < 0) {
-				beefyMotor.setControl(positionVoltage.withPosition(ROTATIONS_DOWN.baseUnitMagnitude())); // Go up
-			} else {
-				beefyMotor.setControl(positionVoltage.withPosition(currentPosition.baseUnitMagnitude())); // Hold position
-			}
-		});
+	public Command climbCommand() {
+		return this.run(() -> {
+				beefyMotor.setControl(new VelocityVoltage(100));
+			})// 	() -> // .until(
+			// 		beefyMotor.getPosition().getValue().compareTo(ROTATIONS_DOWN) > 0 &&
+			// 		beefyMotor.getSupplyCurrent().getValue().compareTo(CURRENT_THRESHOLD) < 0
+			// )
+			.andThen(() -> {
+				beefyMotor.setControl(new VelocityVoltage(0));
+			});
 	}
 
 	public Command descendCommand() {
 		return run(() -> {
-			Angle currentPosition = beefyMotor.getPosition().getValue();
-			Current current = beefyMotor.getSupplyCurrent().getValue();
-			if (
-				currentPosition.compareTo(Angle.ofBaseUnits(0, Degrees)) > 0 && current.compareTo(CURRENT_THRESHOLD) < 0
-			) {
-				beefyMotor.setControl(positionVoltage.withPosition(0)); // Go down
-			} else {
-				beefyMotor.setControl(positionVoltage.withPosition(currentPosition.baseUnitMagnitude())); // Hold position
-			}
+			beefyMotor.setControl(new VelocityVoltage(-100));
+		})// 	() -> // .until(
+		// 		beefyMotor.getPosition().getValue().compareTo(ROTATIONS_DOWN) > 0 &&
+		// 		beefyMotor.getSupplyCurrent().getValue().compareTo(CURRENT_THRESHOLD) < 0
+		// )
+		.andThen(() -> {
+			beefyMotor.setControl(new VelocityVoltage(0));
 		});
 	}
 
