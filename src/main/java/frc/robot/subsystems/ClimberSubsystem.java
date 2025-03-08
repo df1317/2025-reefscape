299d2352c179e7839802060cf8ff54cf179a1fa7
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2025-03-08 11:02:50 -0500

    feat: use voltage control

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -6,9 +6,13 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.configs.AudioConfigs;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,14 +23,26 @@ public class ClimberSubsystem extends SubsystemBase {
 	private final TalonFX beefyMotor;
 	private final Orchestra marioUnderwater;
 	private final Orchestra duckOrchestra;
+	private final VoltageOut m_voltage = new VoltageOut(0).withEnableFOC(false);
 
 	public ClimberSubsystem() {
 		beefyMotor = new TalonFX(CanConstants.beefyMotor);
 
+		HardwareLimitSwitchConfigs newHardwareLimitSwitch = new HardwareLimitSwitchConfigs()
+			.withForwardLimitEnable(false)
+			.withReverseLimitEnable(false)
+			.withForwardLimitSource(ForwardLimitSourceValue.Disabled)
+			.withReverseLimitSource(ReverseLimitSourceValue.Disabled);
+		CurrentLimitsConfigs currentConfigs = new CurrentLimitsConfigs()
+			.withStatorCurrentLimit(30)
+			.withStatorCurrentLimitEnable(true);
 		AudioConfigs audioConfigs = new AudioConfigs().withAllowMusicDurDisable(true);
-		TalonFXConfiguration config = new TalonFXConfiguration().withAudio(audioConfigs);
+		TalonFXConfiguration config = new TalonFXConfiguration()
+			.withHardwareLimitSwitch(newHardwareLimitSwitch)
+			.withAudio(audioConfigs)
+			.withCurrentLimits(currentConfigs);
 
-		config.Slot0.kP = 0.11;
+		config.Slot0.kP = 2;
 		config.Slot0.kI = 0;
 		config.Slot0.kD = 0;
 		beefyMotor.getConfigurator().apply(config);
@@ -52,19 +68,17 @@ public class ClimberSubsystem extends SubsystemBase {
 
 	public Command climbCommand() {
 		return this.run(() -> {
-				beefyMotor.setControl(new VelocityVoltage(100));
-			})// ) // 		beefyMotor.getSupplyCurrent().getValue().compareTo(CURRENT_THRESHOLD) < 0 // 		beefyMotor.getPosition().getValue().compareTo(ROTATIONS_DOWN) > 0 && // 	() -> // .until(
-			.andThen(() -> {
-				beefyMotor.setControl(new VelocityVoltage(0));
+				beefyMotor.setControl(m_voltage.withOutput(-10));
+			}).andThen(() -> {
+				beefyMotor.setControl(m_voltage.withOutput(0));
 			});
 	}
 
 	public Command descendCommand() {
 		return run(() -> {
-			beefyMotor.setControl(new VelocityVoltage(-100));
-		})// ) // 		beefyMotor.getSupplyCurrent().getValue().compareTo(CURRENT_THRESHOLD) < 0 // 		beefyMotor.getPosition().getValue().compareTo(ROTATIONS_DOWN) > 0 && // 	() -> // .until(
-		.andThen(() -> {
-			beefyMotor.setControl(new VelocityVoltage(0));
+			beefyMotor.setControl(m_voltage.withOutput(10));
+		}).andThen(() -> {
+			beefyMotor.setControl(m_voltage.withOutput(0));
 		});
 	}
 
