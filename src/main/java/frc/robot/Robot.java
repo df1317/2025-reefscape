4bcdcf973bb2a4158d06998c32516ee3ed4c907e
PythonAtSea <HudsonGreen27@gmail.com>
2025-10-06 19:21:04 -0400

    chore: install advantage kit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project, you must also update the build.gradle file in the project.
  * ---
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
 
 	private static Robot instance;
 	private Command m_autonomousCommand;
